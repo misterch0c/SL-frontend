657623b2898953b1b4e490061f76f2a86a62d2e1
MisterCh0c <wojhack@unseen.is>
2015-04-06 22:13:29 +0200

    .

--- app/scripts/controllers/addlink.js
+++ app/scripts/controllers/addlink.js
@@ -2,16 +2,28 @@
 
 
 angular.module('yoslApp')
-  .controller('AddlinkCtrl', function ($scope,$http) {
+  .controller('AddlinkCtrl', function ($scope,$resource) {
     console.log("addlink controler");
+    console.log($scope.title);
+
+      	$scope.test = function () {
+   			 $resource('http://localhost:1337/link',{},{}).query(function(data){
+   			 	console.log(data);
+   			  
+   			 });      
+		};      	
+
+		//Get returns an object, query returns an array
+		$scope.addLink = function () {
+   			 $resource('http://localhost:1337/link/create',{title:$scope.title},{'create':{method:'POST',isArray:false}}).get(function(data){
+   			 	console.log(data);
+
+   			  
+   			 });      
+		};
 
 
-      	$scope.test = function ($scope) {
-   			 $http.get('http://localhost:1337/link').
-       		 success(function(data) {
-            console.log(data);
-        });
-	};
+
 
   });
 
