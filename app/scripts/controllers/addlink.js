8748c021b3b2345ed4a20dc437394e72ffd1616e
MisterCh0c <wojhack@unseen.is>
2015-04-07 19:18:05 +0200

    Form fonctionel

--- app/scripts/controllers/addlink.js
+++ app/scripts/controllers/addlink.js
@@ -2,29 +2,39 @@
 
 
 angular.module('yoslApp')
-  .controller('AddlinkCtrl', function ($scope,$resource) {
-    console.log("addlink controler");
-    console.log($scope.title);
+    .controller('AddlinkCtrl', function($scope, $resource) {
+        console.log("addlink controler");
+        console.log($scope.title);
 
-      	$scope.test = function () {
-   			 $resource('http://localhost:1337/link',{},{}).query(function(data){
-   			 	console.log(data);
-   			  
-   			 });      
-		};      	
+        $scope.test = function() {
+            $resource('http://localhost:1337/link', {}, {}).query(function(data) {
+                console.log(data);
 
-		//Get returns an object, query returns an array
-		$scope.addLink = function () {
-   			 $resource('http://localhost:1337/link/create',{title:$scope.title},{'create':{method:'POST',isArray:false}}).get(function(data){
-   			 	console.log(data);
+            });
+        };
 
-   			  
-   			 });      
-		};
+        //Get returns an object, query returns an array
+        $scope.addLink = function() {
+            $resource('http://localhost:1337/link/create', {
+                link: $scope.link,
+                title: $scope.title,
+                description: $scope.description,
+                type: $scope.type,
+                lang: $scope.lang
+            }, {
+                'create': {
+                    method: 'POST',
+                    isArray: false
+                }
+            })
+                .get(function(data) {
+                    console.log($scope.title);
+
+
+                });
+        };
 
 
 
 
-  });
-
-
+    });
