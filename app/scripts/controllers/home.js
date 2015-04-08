9e97999a10ad8796a8dbe3b328537a1c3e90e11e
robin.poncin <robin.poncin@gmail.com>
2015-04-08 16:55:32 +0200

    Ajoute d'un resource factory (Links) permettant des custom get sur les links (by lang et by type)

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,20 +1,35 @@
 'use strict';
 
 
-angular.module('yoslApp')
-  .controller('HomeCtrl', function ($scope,$resource) {
+var app = angular.module('yoslApp');
+
+app.factory('Links' , function($resource) {
+return $resource('http://localhost:1337/link', null,
+    {
+        'whereLang': { method:'GET', params:{lang:'@lang'}, isArray:true },
+        'whereType': { method:'GET', params:{type:'@type'}, isArray:true },
+        'where': { method:'GET', params:{lang:'@lang', type:'@type'}, isArray:true }
+    });
+});
+
+app.controller('HomeCtrl', function ($scope, Links) {
     console.log("home controler");
-
-   			 $resource('http://localhost:1337/link',{},{}).query(function(data){
-   			 	$scope.links=data;
-          console.log($scope.links);
-   			  
-   			 });      
+	
+	//Get links by lang (us)
+	$scope.links = Links.whereLang({lang:'us'});
 		
+	//Get links by type (board)
+	//$scope.links = Links.whereType({type:'board'});
 
-
-
-
+	//Get links by lang and by type
+	//$scope.links = Links.where({lang:'us', type:'board'});
+	
+	/* 
+	  $resource('http://localhost:1337/link',{},{}).query(function(data){
+		$scope.links=data;
+		console.log($scope.links);   			  
+	 });      
+	*/
   });
 
 
