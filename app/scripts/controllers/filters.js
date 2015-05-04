d1328b3e53248955362df9f5ca0613a5ed834e94
robin.poncin <robin.poncin@gmail.com>
2015-05-04 20:03:33 +0200

    Filtres sortBy et hide show links + réarrangement panel view (size, tags..)

--- app/scripts/controllers/filters.js
+++ app/scripts/controllers/filters.js
@@ -10,13 +10,12 @@ app.controller('FiltersCtrl', function($rootScope, $scope) {
 		$('#switch').bootstrapSwitch('state', $rootScope.show);
 */
 		$('#switch').on('switchChange.bootstrapSwitch', function(event, state) {
-				$rootScope.show = state;				
-				$rootScope.$apply();
-
+			$rootScope.show = state;				
+			$rootScope.$apply();
 		});    
 
 		$(document).ready(function() {
-			$('[name="switch"]').bootstrapSwitch();
+			$('[name="switch"]').bootstrapSwitch('state', $rootScope.show);
 		});
 
 /*
@@ -26,11 +25,20 @@ app.controller('FiltersCtrl', function($rootScope, $scope) {
 */
 		$rootScope.lang = "";
 		$scope.lang = "";
-
 		$rootScope.changeLang = function(){
 			$rootScope.lang = $scope.lang;
 		};
 
+		$rootScope.sortBy = "title";
+		$scope.sortBy = "title";
+		$rootScope.changeSortBy = function(){
+			$rootScope.sortBy = $scope.sortBy;
+		};
+
+		$rootScope.hideDownLinks = false;
+		$rootScope.toggleHideDown = function() {
+			$rootScope.hideDownLinks = !$rootScope.hideDownLinks;
+		};
 /*
 			++$scope.ind;
 			if ($scope.ind >= Object.keys($scope.langs).length){
