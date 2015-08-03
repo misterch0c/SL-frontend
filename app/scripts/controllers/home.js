6063b2de52ec046a5b235be3e056f4522e02d61a
misterch0c <wojhack@unseen.is>
2015-08-03 16:13:38 +0000

    Dirty css

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -3,7 +3,7 @@
 var app = angular.module('yoslApp');
 
 app.factory('Links', function($resource) {
-    return $resource('http://localhost:1337/link', null, {
+    return $resource('http://localhost:1337/link?limit=0', null, {
         'whereLang': {
             method: 'GET',
             params: {
@@ -41,80 +41,81 @@ app.controller('HomeCtrl', function($scope, Links) {
     });
 
     //Get links by lang (us)
-    // $scope.links = Links.query();
-    $scope.links = [
-        {
-            link:"google.com",
-            title:"Zenk-Security",
-            description:"Zenk Security est une communauté de hacking et de sécurité informatique francophone basé sur le partage et l'apprentissage.",
-            lang: "fr",
-            rank: 1,
-            deltaRank: 0,
-            isup:true,
-            tags : ["tag5", "tag2", "tag3", "exampleTag"]
-        },
-        {
-            link:"google.com",
-            title:"222222222222",
-            description:"Search Engine",
-            lang: "fr",
-            rank: 10,
-            deltaRank: +2,
-                        isup:false,
-            tags : ["tag1", "tag2", "tag3", "exampleTag"]
-        },
-        {
-            link:"google.com",
-            title:"3333333333333",
-            description:"Search Engine",
-            lang: "fr",
-            rank: 7,
-            deltaRank: -1,
-                        isup:true,
-            tags : ["tag1", "tag2", "tag3", "exampleTag"]
-        },
-        {
-            link:"google.com",
-            title:"Google",
-            description:"Search Engine",
-            lang: "us",
-            rank: 6,
-            deltaRank: +1,
-                        isup:true,
-            tags : ["tag1", "tag2", "tag3", "exampleTag"]
-        },
-        {
-            link:"godfogle.com",
-            title:"dfd",
-            description:"kjhk Engine",
-            lang: "us",
-            rank: 6,
-            deltaRank: +1,
-                        isup:false,
-            tags : ["tag1", "tag2", "tag3", "exampleTag"]
-        },
-        {
-            link:"sdqsd.com",
-            title:"qsdqsd",
-            description:"qsd SDqsd ",
-            lang: "es",
-            rank: 6,
-            deltaRank: +1,
-                        isup:false,
-            tags : ["tag1", "tag2", "tag3", "exampleTag"]
-        },
-        {
-            link:"sdfdsf.com",
-            title:"dsf f hacking",
-            description:"sdfg  kiug",
-            lang: "es",
-            rank: 6,
-            deltaRank: +1,
-                        isup:true,
-            tags : ["tag1", "tag2", "tag3", "exampleTag"]
-        },
+     $scope.links = Links.query();
+     console.log($scope.links);
+    // $scope.links = [
+    //     {
+    //         link:"google.com",
+    //         title:"Zenk-Security",
+    //         description:"Zenk Security est une communauté de hacking et de sécurité informatique francophone basé sur le partage et l'apprentissage.",
+    //         lang: "fr",
+    //         rank: 1,
+    //         deltaRank: 0,
+    //         isup:true,
+    //         tags : ["tag5", "tag2", "tag3", "exampleTag"]
+    //     },
+    //     {
+    //         link:"google.com",
+    //         title:"222222222222",
+    //         description:"Search Engine",
+    //         lang: "fr",
+    //         rank: 10,
+    //         deltaRank: +2,
+    //                     isup:false,
+    //         tags : ["tag1", "tag2", "tag3", "exampleTag"]
+    //     },
+    //     {
+    //         link:"google.com",
+    //         title:"3333333333333",
+    //         description:"Search Engine",
+    //         lang: "fr",
+    //         rank: 7,
+    //         deltaRank: -1,
+    //                     isup:true,
+    //         tags : ["tag1", "tag2", "tag3", "exampleTag"]
+    //     },
+    //     {
+    //         link:"google.com",
+    //         title:"Google",
+    //         description:"Search Engine",
+    //         lang: "us",
+    //         rank: 6,
+    //         deltaRank: +1,
+    //                     isup:true,
+    //         tags : ["tag1", "tag2", "tag3", "exampleTag"]
+    //     },
+    //     {
+    //         link:"godfogle.com",
+    //         title:"dfd",
+    //         description:"kjhk Engine",
+    //         lang: "us",
+    //         rank: 6,
+    //         deltaRank: +1,
+    //                     isup:false,
+    //         tags : ["tag1", "tag2", "tag3", "exampleTag"]
+    //     },
+    //     {
+    //         link:"sdqsd.com",
+    //         title:"qsdqsd",
+    //         description:"qsd SDqsd ",
+    //         lang: "es",
+    //         rank: 6,
+    //         deltaRank: +1,
+    //                     isup:false,
+    //         tags : ["tag1", "tag2", "tag3", "exampleTag"]
+    //     },
+    //     {
+    //         link:"sdfdsf.com",
+    //         title:"dsf f hacking",
+    //         description:"sdfg  kiug",
+    //         lang: "es",
+    //         rank: 6,
+    //         deltaRank: +1,
+    //                     isup:true,
+    //         tags : ["tag1", "tag2", "tag3", "exampleTag"]
+    //     },
 
-    ];
+    // ];
 
     $scope.languages = ['fr','us','es'];
     $scope.fullLanguageName = function(name){
